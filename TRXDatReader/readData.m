8cf531f070f977b678d8e8c1d52d7e7a08962197
Timo Rantalainen <tjrantal@gmail.com>
2013-05-11 10:20:49 +0300

    Waitbar updated only every 10 packages, works better with Octave

--- TRXDatReader/readData.m
+++ TRXDatReader/readData.m
@@ -4,13 +4,16 @@
         data = [];
         packetNo = 0;
         wb = waitbar(0,'reading packets');
+        
         while pointer < length(dataToMem)
             packet = readPacket(dataToMem);
             pointer = pointer+512;  %Packet size = 512 bytes + 2 bytes for aa -code of the packet
             packetNo = packetNo+1;
             data = [data, packet];%.packet(packetNo) =packet;
 %             keyboard
-            waitbar(pointer/length(dataToMem),wb);
+			if pointer/length(dataToMem) < 1 && mod(packetNo,20) == 0
+            	waitbar(pointer/length(dataToMem),wb);
+            end
         end
         close(wb);
     end
--- testSingleFile.m
+++ testSingleFile.m
@@ -9,7 +9,7 @@ figure
 colourCodes = {'k','r','b'};
 for ii = 1:3
     subplot(4,1,ii)
-    plot(data.data(ii+1,:),colourCodes{ii})
+    plot(data.data(ii+1,:)*32/2^13,colourCodes{ii})
 end
 subplot(4,1,4)
 plot(data.data(1,:),'k')
