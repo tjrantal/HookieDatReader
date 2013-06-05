b490233cef8c3a445819faa689317597cc6637a1
tjrantal <tjrantal@gmail.com>
2013-06-05 14:42:24 +0300

    Figured out timestamps

--- testSingleFile.m
+++ testSingleFile.m
@@ -3,14 +3,15 @@ close all;
 fclose all;
 clc;
 addpath('TRXDatReader');
-fileToRead = 'DATA0002.DAT';
+% fileToRead = 'DATA0002.DAT';
+fileToRead = 'C:\MyTemp\oma\Timon\tyo\AquaRehab2012\Analysis\Accelerometry\Koe_phase I\AH170246\DATA0004.DAT';
 data = readTRXDat(fileToRead);
 figure
 colourCodes = {'k','r','b'};
 for ii = 1:3
     subplot(4,1,ii)
-    plot(data.data(ii+1,:)*32/2^13,colourCodes{ii})
+    plot(data.data.values(ii+1,:)*32/2^13,colourCodes{ii})
 end
 subplot(4,1,4)
-plot(data.data(1,:),'k')
+plot(data.data.values(1,:),'k')
 
