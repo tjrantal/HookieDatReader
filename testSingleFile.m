ccc6b76e02a12fb69636674213f87791a101a94e
tjrantal <tjrantal@gmail.com>
2013-06-06 11:01:11 +0300

    Added sample

--- testSingleFile.m
+++ testSingleFile.m
@@ -1,27 +1,9 @@
+%Read a HOOKIE file sample
 clear all;
 close all;
 fclose all;
 clc;
 addpath('TRXDatReader');
-% parameters.lahde = 'C:\MyTemp\oma\Timon\tyo\AquaRehab2012\Analysis\Accelerometry\Koe_phase I\AH170246';
-% fileToRead = 'DATA0002.DAT';
-fileToRead = 'C:\MyTemp\oma\Timon\tyo\AquaRehab2012\Analysis\Accelerometry\Koe_phase I\AH170246\DATA0004.DAT';% fileToRead = 'C:\MyTemp\oma\Timon\tyo\AquaRehab2012\Analysis\Accelerometry\Koe_phase I\AH170246\DATA0022.DAT';
-% D = dir([parameters.lahde '\*.dat']);
-% mones = length(D);
-% tietoja = 10000;
-% for iii = 1:length(D)
-%     data = readTRXDat([parameters.lahde '\' D(iii).name]);
-    data = readTRXDat(fileToRead);
-    figure
-    colourCodes = {'k','r','b'};
-    titleText = {'X','Y','Z'};
-    for ii = 1:3
-        subplot(4,1,ii)
-        plot((data.data.values(ii+1,:)/2^12*16),colourCodes{ii})
-        title(titleText{ii});
-    end
-    %Plot timeStamps
-    subplot(4,1,4)
-    plot(data.data.values(1,:))
-    title('Timestamps as datenums');
-% end
+fileToRead = 'C:\path\to\your\file\DATAFILE.DAT';
+data = readTRXDat(fileToRead);          %Read the data from the file
+fullData = insertInactivity(data,48);   %Insert the inactivity periods The second parameter limits the amount of data to 48 h. I tried to reconstruct a file with 120+ h of data, which crashed my machine...
--- testSingleFileTimeInstants.m
+++
@@ -1,9 +0,0 @@
-%Sample code for testing the reader
-clear all;
-close all;
-fclose all;
-clc;
-addpath('TRXDatReader');
-fileToRead = 'C:\Path\to\HookieFile\DATAFILE.DAT';
-data = readTRXDat(fileToRead);
-fullData = insertInactivity(data,48);
