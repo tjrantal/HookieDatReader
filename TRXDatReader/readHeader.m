e11342aec437859cb531d55cd14a89e9d089f5a9
tjrantal <tjrantal@gmail.com>
2013-06-19 10:49:54 +0300

    Added a fix for reading a file with a missing header

--- TRXDatReader/readHeader.m
+++ TRXDatReader/readHeader.m
@@ -5,23 +5,27 @@
         %Read measurement date
         stringToLookFor = 'date';
         pointer = lookForString(dataToMem,stringToLookFor,pointer);
-        header.date = char(dataToMem(pointer+length(stringToLookFor)+2:pointer+length(stringToLookFor)+2+9)');
-        pointer = pointer+length(stringToLookFor)+2+9;
-        %Get measurement time
-        stringToLookFor = 'time';
-        pointer = lookForString(dataToMem,stringToLookFor,pointer);
-        header.time = char(dataToMem(pointer+length(stringToLookFor)+2:pointer+length(stringToLookFor)+2+7)');
-        pointer = pointer+length(stringToLookFor)+2+7;
-        %Get sampling rate
-        stringToLookFor = 'data rate';
-        pointer = lookForString(dataToMem,stringToLookFor,pointer);
-        header.samplingRate = char(dataToMem(pointer+length(stringToLookFor)+2:pointer+length(stringToLookFor)+2+5)');
-        pointer = pointer+length(stringToLookFor)+2+5;
-        %Get compression
-        stringToLookFor = 'data compression';
-        pointer = lookForString(dataToMem,stringToLookFor,pointer);
-        header.compression = char(dataToMem(pointer+length(stringToLookFor)+2:pointer+length(stringToLookFor)+2+2)');
-        pointer = pointer+length(stringToLookFor)+2+2;
+        if pointer < length(dataToMem)-3
+            header.date = char(dataToMem(pointer+length(stringToLookFor)+2:pointer+length(stringToLookFor)+2+9)');
+            pointer = pointer+length(stringToLookFor)+2+9;
+            %Get measurement time
+            stringToLookFor = 'time';
+            pointer = lookForString(dataToMem,stringToLookFor,pointer);
+            header.time = char(dataToMem(pointer+length(stringToLookFor)+2:pointer+length(stringToLookFor)+2+7)');
+            pointer = pointer+length(stringToLookFor)+2+7;
+            %Get sampling rate
+            stringToLookFor = 'data rate';
+            pointer = lookForString(dataToMem,stringToLookFor,pointer);
+            header.samplingRate = char(dataToMem(pointer+length(stringToLookFor)+2:pointer+length(stringToLookFor)+2+5)');
+            pointer = pointer+length(stringToLookFor)+2+5;
+            %Get compression
+            stringToLookFor = 'data compression';
+            pointer = lookForString(dataToMem,stringToLookFor,pointer);
+            header.compression = char(dataToMem(pointer+length(stringToLookFor)+2:pointer+length(stringToLookFor)+2+2)');
+            pointer = pointer+length(stringToLookFor)+2+2;
+        else
+            pointer = 1;
+        end
         %Set the pointer to the beginning of the first data packet
         stringToLookFor = [char(uint8(hex2dec('AA'))), char(uint8(hex2dec('AA')))];
         pointer = lookForString(dataToMem,stringToLookFor,pointer);
